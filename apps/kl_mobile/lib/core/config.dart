4619ede89f6fc280295e480cebc7fc6c3f02ae15
Nihar Thakkar <nihartar@gmail.com>
2025-10-06 15:24:15 +0200

    chore: cleanup

--- apps/kl_mobile/lib/core/config.dart
+++
@@ -1,7 +0,0 @@
-class Settings {
-  // API service settings
-  // TODO: Load this dynamically from an env file or during compile-time
-  static const String apiBaseUrl = 'http://localhost:8000/v1/';
-  static const int apiConnectTimeout = 30;
-  static const int apiReceiveTimeout = 30;
-}
