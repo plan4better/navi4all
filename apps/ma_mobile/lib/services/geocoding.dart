9f6c77e2b54f394b9c563bce64642a3eb162763d
Nihar Thakkar <nihartar@gmail.com>
2025-11-04 15:58:06 +0100

    refactor: build search controller

--- apps/ma_mobile/lib/services/geocoding.dart
+++ apps/ma_mobile/lib/services/geocoding.dart
@@ -1,21 +1,34 @@
 import 'package:dio/dio.dart';
+import 'package:smartroots/schemas/routing/place.dart';
 import 'package:smartroots/services/api.dart';
 
 class GeocodingService extends APIService {
-  Future<Response> autocomplete({
+  Future<(DateTime, List<Place>)> autocomplete({
     required String timestamp,
     required String query,
     double? focusPointLat,
     double? focusPointLon,
     int limit = 5,
-  }) async => apiClient.get(
-    '/geocoding/autocomplete',
-    queryParameters: {
-      'timestamp': timestamp,
-      'query': query,
-      if (focusPointLat != null) 'focus_point_lat': focusPointLat,
-      if (focusPointLon != null) 'focus_point_lon': focusPointLon,
-      'limit': limit,
-    },
-  );
+  }) async {
+    Response response = await apiClient.get(
+      '/geocoding/autocomplete',
+      queryParameters: {
+        'timestamp': timestamp,
+        'query': query,
+        if (focusPointLat != null) 'focus_point_lat': focusPointLat,
+        if (focusPointLon != null) 'focus_point_lon': focusPointLon,
+        'limit': limit,
+      },
+    );
+
+    if (response.statusCode != 200) {
+      throw Exception(response.statusMessage);
+    }
+    return (
+      DateTime.parse(response.data['timestamp']),
+      (response.data['results'] as List)
+          .map((item) => Place.fromJson(item))
+          .toList(),
+    );
+  }
 }
